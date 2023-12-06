38425f97e026cf9861d9545164bc10ec93c16f3e
Edwin A. Collazos <eacl.831@gmail.com>
2023-12-06 15:55:33 -0500

    [FinalExam]: adds HTML controllers and logic in the code behind class to validate user input in text box according to requirements

--- WDD_Final-Practical-Component/Page1.aspx.cs
+++ WDD_Final-Practical-Component/Page1.aspx.cs
@@ -1,17 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
+using System.Web.Services.Protocols;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace WDD_Final_Practical_Component
 {
-    public partial class Page1 : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+    public partial class Page1 : Page {
+        private const int EXPECTED_NUMBER_OF_WORDS = 2;
+        private const string PAGE2_URL = "Page2.aspx";
+        protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        protected void NextBtn_Click(object sender, EventArgs e) {
+            // validate that:
+            // The input box isn't empty
+            // only alphabetic letters
+
+            string fullName = FullName.Value;
+            if(string.IsNullOrEmpty(fullName)) {
+                // write to the user the error
+                FeedBack.InnerHtml = "*This field cannot be empty";
+                return;
+            }
+
+            int result = CheckNameFormat(fullName);
+            switch(result) {
+                case 0:
+                    FeedBack.InnerHtml = "";
+                    Response.Redirect(PAGE2_URL);
+                    break;
+                case 1:
+                    FeedBack.InnerHtml = "*Please, type First and Last name separated by a space";
+                    break;
+                case 2:
+                    FeedBack.InnerHtml = "*First name can only contain letters (i.e. Sally)";
+                    break;
+                case 3:
+                    FeedBack.InnerHtml = "*Last name can only contain letters (i.e. Smith)";
+                    break;
+                default:
+                    FeedBack.InnerHtml = "";
+                    break;
+            }
+        }
+
+        private int CheckNameFormat(string fullName) {
+            // get the first and last name by separate
+            // check that we fetched ONLY 2 words   -> First name and Last name
+            string[] nameCollection = fullName.Split(' ');
+            if(nameCollection.Length != EXPECTED_NUMBER_OF_WORDS) {
+                return 1;
+            }
+
+            string firstName = nameCollection[0];
+            string lastName = nameCollection[1];
+
+            if(!firstName.All(Char.IsLetter)) {
+                return 2;
+            } else if(!lastName.All(Char.IsLetter)) {
+                return 3;
+            } else {
+                return 0;
+            }
         }
     }
 }
